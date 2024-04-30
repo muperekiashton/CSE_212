dd0adc517f5e6b6066441775f3fd81ba92a7d2f8
muperekiashton <ashtonmupereki@icloud.com>
2024-04-30 14:00:55 -0700

    added

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -32,15 +32,18 @@ public static class ArraysTester {
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
-    private static double[] MultiplesOf(double number, int length)
-    {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+    public static List<double> MultiplesOf(double startingNum, int numMultiples)
+{
+    List<double> multiples = new List<double>();
 
-        return new double[0]; // replace this return statement with your own
+    for (int i = 0; i < numMultiples; i++)
+    {
+        double currentMultiple = startingNum * (i + 1);
+        multiples.Add(currentMultiple);
     }
+
+    return multiples;
+}
     
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -50,12 +53,14 @@ public static class ArraysTester {
     /// <br /><br />
     /// Because a list is dynamic, this function will modify the existing <c>data</c> list rather than returning a new list.
     /// </summary>
-    private static void RotateListRight(List<int> data, int amount)
-    {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+    public static List<T> RotateListRight<T>(List<T> list, int amount)
+{
+    int rotateAmount = amount % list.Count;
+    List<T> rotatedList = new List<T>();
 
-    }
+    rotatedList.AddRange(list.GetRange(list.Count - rotateAmount, rotateAmount));
+    rotatedList.AddRange(list.GetRange(0, list.Count - rotateAmount));
+
+    return rotatedList;
+}
 }
